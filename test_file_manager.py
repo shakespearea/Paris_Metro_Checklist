4be03106a8123053f7db70c007f2d1ad93e0a49f
shakespearea <shakespeareamber@gmail.com>
2026-03-06 19:21:19 +0100

    [fix] Incorrect test params

--- test_file_manager.py
+++ test_file_manager.py
@@ -3,25 +3,22 @@ from file_manager import load_stations, load_states, save_state
 
 # Initialisation
 @pytest.fixture
-def metro():
-    return load_stations()
-
 def visited():
     return {}
 
-def test_set_sation_visited(metro):
+def test_set_sation_visited(visited):
     assert visited.get("Pasteur", False) is False
     visited["Pasteur"] = True
     assert visited["Pasteur"] is True
 
-def test_save_and_reload(metro, tmp_path):
+def test_save_and_reload(visited, tmp_path):
     temp_states = tmp_path / "temp_states.json"
     visited["Pasteur"] = True
     save_state(visited, temp_states)
     states = load_states(temp_states)
     assert states["Pasteur"] is True
 
-def test_unsaved_change_does_not_affect_saved_state(metro, tmp_path):
+def test_unsaved_change_does_not_affect_saved_state(visited, tmp_path):
     temp_states = tmp_path / "temp_states.json"
     visited["Pasteur"] = True
     save_state(visited, temp_states)
